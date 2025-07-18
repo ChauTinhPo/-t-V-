4ba216611d6ce8e85635d00bb8d26e7464ddb094
ChauTinhPo <163418540+ChauTinhPo@users.noreply.github.com>
2025-07-18 07:32:25 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using DO_AN.Models;
+using DO_AN.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services 
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSession();
+builder.Services.AddSingleton<IVNPayService, VNPayService>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<ReportService>();
+
+// Inject IConfiguration to access appsettings.json
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<DOAN_BoSungContext>(opts =>
+{
+    opts.UseSqlServer(configuration.GetConnectionString("DoAnConnection"));
+});
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+})
+.AddEntityFrameworkStores<DOAN_BoSungContext>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Sales}/{action=DailyRevenueChart}/{id?}"
+    );
+
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=TrangChu}/{id?}"
+       
+    );
+
+});
+
+
+
+
+
+app.Run();
